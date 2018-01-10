ed05e3c0b508264c43d1857375121864beb48ce3
buzheng <yupeibiaodeveloper@yeah.net>
2018-01-11 02:28:40 +0800

    更新一下

--- MNMall/settings.py
+++ MNMall/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mall',
         'USER': 'root',
-        'PASSWORD': 'mogujie2016',
+        'PASSWORD': '密码',
 
     }
 }
